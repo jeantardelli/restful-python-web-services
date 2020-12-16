927b6c149c2f9243bde8f1e4bb92ba5979ec61b7
jeantardelli <jeantardelli@gmail.com>
2020-12-16 18:39:50 +0100

    RESTful API interact with a database - Combining blueprints with resource routing (2d)

--- database-models-interaction/service/config.py
+++ database-models-interaction/service/config.py
@@ -5,35 +5,23 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-    SQLALCHEMY_ECHO = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    DATABASE_USER = os.environ.get('DB_USER')
-    DATABASE_PASS = os.environ.get('DB_PASS')
-    DATABASE_ADDR = os.environ.get('DB_ADDR')
-    DATABASE_DEV = os.environ.get('DB_DEV')
-    DATABASE_PROD = os.environ.get('DB_PROD') 
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+DATABASE_USER = os.environ.get('DB_USER')
+DATABASE_PASS = os.environ.get('DB_PASS')
+DATABASE_ADDR = os.environ.get('DB_ADDR')
+DATABASE_DEV = os.environ.get('DB_DEV')
+DATABASE_PROD = os.environ.get('DB_PROD') 
 
-    @staticmethod
-    def init_app(app):
-        pass
-
-class DevelopmentConfig(Config):
+if not DATABASE_DEV:
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{user}:{pwd}@{addr}/{db_dev}".format(
         user=Config.DATABASE_USER,
         pwd=Config.DATABASE_PASS,
         addr=Config.DATABASE_ADDR,
         db_dev=Config.DATABASE_DEV)
-
-class ProductionConfig(Config):
+else:
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{user}:{pwd}@{addr}/{db_prod}".format(
         user=Config.DATABASE_USER,
         pwd=Config.DATABASE_PASS,
         addr=Config.DATABASE_ADDR,
         db_prod=Config.DATABASE_PROD)
-
-config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'default': ProductionConfig
-    }
---
+++ database-models-interaction/service/views.py
@@ -0,0 +1,188 @@
+"""
+This module contain code that creates the resources that compose the building
+blocks for the RESTful API.
+"""
+from flask import Blueprint, request, jsonify, make_response
+from flask_restful import Api, Resource
+from http_status import HttpStatus
+from models import db, NotificationCategory, NotificationCategorySchema, \
+        Notification, NotificationSchema
+from sqlalchemy.exc import SQLAlchemyError
+
+service_blueprint = Blueprint('service', __name__)
+notification_category_schema = NotificationCategorySchema
+notification_schema = NotificationSchema()
+service = Api(service_blueprint)
+
+class NotificationResource(Resource):
+    def get(self, id):
+        notification = Notification.query.get_or_404(id)
+        dumped_notification = notification_schema.dump(notification).data
+        return dumped_notification
+
+    def patch(self, id):
+        notification = Notification.query.get_or_404(id)
+        notification_dict = request.get_json(force=True)
+
+        if 'message' in notification_dict and \
+                notification_dict['message'] is not None:
+            notification.message = notification_dict['messge']
+        if 'ttl' in notification_dict and \
+                notification['ttl'] is not None:
+            notification.duration = notification_dict['duration']
+        if 'displayed_times' in notification_dict and \
+                notification_dict['displayed_times'] is not None:
+            notification.displayed_times = notification_dict['displayed_times']
+        if 'displayed_once' in notification_dict and \
+                notification_dict['displayed_once'] is not None:
+            notification.displayed_once = notification_dict['displayed_once']
+
+        dumped_notification, dump_errors = notification_schema.dump(message)
+        if dump_errors:
+            return dump_errors, HttpStatus.bad_request_400.value
+
+        validate_errors = notification_schema.validate(dumped_notification)
+        if validate_errors:
+            return validate_errors, HttpStatus.bad_request_400.value
+
+        try:
+            notification.update()
+            return self.get(id)
+        except SQLAlchemaError as err:
+            db.session.rollback()
+            response = {"error": str(err)}
+            return response, HttpStatus.bad_request_400.value
+
+    def delete(self, id):
+        notification = Notification.query.get_or_404(id)
+
+        try:
+            delete = notification.delete(notification)
+            response = make_response()
+            return response, HttpStatus.no_content_204.value
+        except SQLAlchemy as err:
+            db.session.rollback()
+            response = {"error": str(err)}
+            return response, HttpStatus.unauthorized_401.value
+
+class NotificationListResource(Resource):
+    def get(self):
+        notifications = Notification.query.all()
+        dump_result = notification_schema.dump(notifications, many=True).data
+        return dump_result
+
+    def post(self):
+        notification_category_dict = request.get_json()
+
+        if not notification_category_dict:
+            response = {"message": "No input data provided"}
+            return response, HttpStatus.bad_request_400.value
+
+        errors = notification_schema.validate(notification_category_dict)
+        if errors:
+            return errors, HttpStatus.bad_request_400.value
+
+        try:
+            notification_category_name = notification_category_dict\
+                    ['notification_category']['name']
+            notification_category = NotificationCategory.query\
+                    .filter_by(name=notification_category_name).first()
+
+            if notification_category is None:
+                # Create a new NotificationCategory
+                notification_category = NotificationCategory(
+                    name=notification_category_name)
+                db.session.add(
+                    notification_category)
+
+            # Now we can create a new Notification
+            notification = Notification(
+                message=notification_category_dict['message'],
+                ttl=notification_category_dict['ttl'],
+                notification_category=notification_category)
+            notification.add(notification)
+
+            query = Notification.query.get(notification.id)
+            dump_result = notification_schema.dump(query).data
+            return dump_result, HttpStatus.created_201.value
+        except SQLAlchemy as err:
+            db.session.rollback()
+            response = {"error": str(err)}
+            return response, HttpStatus.bad_request_400.value
+
+class NotificationCategoryResource(Resource):
+    def get(self, id):
+        notification_category = NotificationCategory.query.get_or_404(id)
+        dump_result = notification_category_schema.dump(notification_category)\
+            .data
+        return dump_result
+
+    def patch(self, id):
+        notification_category = NotificationCategory.query.get_or_404(id)
+        notification_category_dict = request.get_json()
+
+        if not notification_category_dict:
+            response = {'message': 'No input data provided'}
+            return response, HttpStatus.bad_request_400.value
+
+        errors = notification_category_schema.validate(notification_category_dict)
+        if errors:
+            return errors, HttpStatus.bad_request_400.value
+
+        try:
+            if 'name' in notification_category_dict and \
+                    notification_category_dict['name'] is not None:
+                notification_category.name = notification_category_dict['name']
+            notification_category.update()
+            return self.get(id)
+        except SQLAlchemyError as err:
+            db.session.rollback()
+            response = {'error': str(err)}
+            return response, HttpStatus.bad_request_400.value
+
+    def delete(self, id):
+        notification_category = NotificationCategory.query.get_or_404(id)
+
+        try:
+            notification_category.delete(notification_category)
+            response = make_response()
+            return response, HttpStatus.no_content_204.value
+        except SQLAlchemy as err:
+            db.session.rollback()
+            response = {'error': str(err)}
+            return response, HttpStatus.unauthorized_401.value
+
+class NotificationCategoryListResource(Resource):
+    def get(self):
+        notification_categories = NotificationCategory.query.all()
+        dump_results = notification_category_schema.dump(
+            notification_categories, many=True).data
+        return dump_results
+
+    def post(self):
+        print("Processing")
+
+        notification_category_dict = request.get_json()
+        if not notification_category_dict:
+            response = {'message': 'No input data provided'}
+            return response, HttpStatus.bad_request_400.value
+
+        errors = notification_category_schema.validate(
+            notification_category_dict)
+        if errors:
+            return errors, HttpStatus.bad_request_400.value
+
+        try:
+            notification_category = NotificationCategory(
+                    notification_category_dict['name'])
+            notification_category.add(notification_category)
+            query = NotificationCategory.query.get(notification_category.id)
+            dump_result = notification_category_schema.dump(query).data
+            return dump_result, HttpStatus.created_201.value
+        except SQLAlchemyError as err:
+            print("Error")
+            print(err)
+            db.session.rollback()
+            response = {"error": str(err)}
+            return response, HttpStatus.bad_request_400.value
+
